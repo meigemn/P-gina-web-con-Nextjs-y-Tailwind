36577aa36f0d174a8ff6e9efa258f2e762296c4c
meigemn <carloscc9297@gmail.com>
2024-11-06 19:43:12 +0100

    added pages: 1, 1.1, 1.1.1

--- src/app/1_1_1_que_necesito_para_poder_convalidar/page.jsx
+++ src/app/1_1_1_que_necesito_para_poder_convalidar/page.jsx
@@ -8,8 +8,15 @@ export const metadata = {
   <header>
     <Titulo>{metadata.title}</Titulo>
   </header>
-  <main>
-    
+  <main className="bg-blue-200 rounded-lg p-4 mx-4 mt-4 text-blue-950">
+  <p className="mb-4 mx-4">La primera condición:</p>
+    <p className="mb-4 justify-end  mx-12">
+      ✅ Si eres alumno de SEMI o distancia: <b>estar matriculado/a en el módulo que se desea convalidar</b> y realizar el pago de las tasas correspondientes. Hasta que no se realice y se compruebe el pago, no se podrá ejecutar la convalidación en el expediente. 
+      De esta forma, si el pago se retrasa cabría la posibilidad de que la resolución de la convalidación también se retrasara.
+    </p>
+    <p className="mb-4 mx-4">    
+      La <b>convalidación no se realiza sobre la globalidad del Título</b> que se está cursando, sino sobre los módulos que se soliciten, si estos cumplen las condiciones para ello.
+    </p>
   </main>
   <nav>
     <Nav posicion={3} />\
--- src/app/1_solicitud_de_convalidaciones/page.jsx
+++ src/app/1_solicitud_de_convalidaciones/page.jsx
@@ -9,7 +9,7 @@ export const metadata = {
       <header>
         <Titulo>{metadata.title}</Titulo>
       </header>
-      <main className="bg-blue-200 rounded-lg p-4 mt-8 ml-4 ">
+      <main className="bg-blue-200 rounded-lg p-4 mt-8 ml-4  text-blue-950">
         <p>Aquí encontrarás información acerca de cómo solicitar la convalidación de un módulo profesional y la exención del módulo de Formación en Centros de Trabajo.</p>
         <div className="mx-auto flex justify-center w-[200px] mt-8">
         <Image layout='responsive'src='/balanza.png' alt="imagen balanza" width="50" height="50"></Image>
