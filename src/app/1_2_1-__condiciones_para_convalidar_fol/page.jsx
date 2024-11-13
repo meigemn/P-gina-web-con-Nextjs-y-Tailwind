d46093857679fac9f72547830b3d0d15afa0d019
meigemn <carloscc9297@gmail.com>
2024-11-13 14:23:33 +0100

    Added one page with table

--- src/app/1_2_1-__condiciones_para_convalidar_fol/page.jsx
+++ src/app/1_2_1-__condiciones_para_convalidar_fol/page.jsx
@@ -7,6 +7,116 @@ export const metadata = {
   return (
     <div>
       <Titulo>{metadata.title}</Titulo>{/**Importamos el componente Titulo */}
+      <div>{/* div principal */}
+        <div className="mt-8 ml-4">{/* comienzo */}
+          <p>Para poder convalidar el módulo de FOL (Formación y Orientación Laboral) 
+            es necesario acreditar alguna de las siguientes condiciones:
+          </p>
+        </div>{/* fin comienzo */}
+
+        <div className="ml-10 mt-4">
+          <p>✔️Si el módulo a convalidar pertenece a un ciclo LOE (Ley Orgánica 2/2006, de 3 de mayo). 
+            <b>Para esta convalidación es indiferente que los módulos que se aporten sean de grado medio o superior</b>.
+          </p>
+        </div>
+        <div className="ml-16 mt-4">
+          <p>➡️<b>Tener aprobado el módulo de FOL en cualquier otro Ciclo LOE y acreditarlo documentalmente</b></p>
+          <p>➡️ <b>Tener aprobado el módulo de FOL en un Ciclo LOGSE (Ley Orgánica 1/1990, de 3 de octubre) y poseer un certificado que acredite que se posee la formación establecida para el desempeño de las funciones de nivel básico de la actividad preventiva </b>
+            (PRL). Sirve cualquier curso de PRL impartido por alguna entidad legalmente constituida, de una duración no inferior a 30 horas y cuyos contenidos incluyan los que indica el anexo IV del Real Decreto 39/1997, de 17 de enero (BOE de 31 de enero de 1997), por el que se aprueba el Reglamento de los Servicios de Prevención.
+              El certificado del curso de PRL deberá hacer constar específicamente tanto la duración como los contenidos impartidos.</p>
+          <p>➡️. <b> Poseer el título de Técnico Superior en Prevención de Riesgos Profesionales derivado de la Ley Orgánica 1/1990, de 3 de octubre</b></p>
+        </div>
+        <div className="ml-10 mt-4">
+        ✔️Si el módulo a convalidar pertenece a un ciclo LOGSE de grado medio (Ley Orgánica 1/1990, de 3 de octubre):
+          <div className="ml-6 mt-4">
+            <p>
+            ➡️  <b>Tener aprobado el módulo de FOL en un Ciclo LOGSE (Ley Orgánica 1/1990, de 3 de octubre).</b>
+            </p>
+            <p>
+            ➡️ <b>Tener aprobado el módulo de FOL en cualquier otro Ciclo LOE y acreditarlo documentalmente.</b>
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-center mt-4">{/* tabla */}
+        <table className=" table-auto border-collapse ">
+        <thead>
+          <tr className="bg-gray-200 text-left">
+            <th className="px-4 py-2 border-b bg-blue-200">Módulo a Convalidar</th>
+            <th className="px-4 py-2 border-b bg-blue-200">Formación a Aportar</th>
+          </tr>
+        </thead>
+        <tbody>
+        {/* primer fol */}
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200 w-40" > <b>FOL GM (LOGSE Ley Orgánica 1/1990, de 3 de octubre)</b></td>
+              <td className="px-4 py-2">FOL GM LOGSE</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"></td>
+              <td className="px-4 py-2">FOL GS LOGSE</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"></td>
+              <td className="px-4 py-2">FOL GM LOE</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"></td>
+              <td className="px-4 py-2">FOL GS LOE</td>
+            </tr>
+         {/* segundo Fol */}
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"> <b>FOL GS (LOGSE Ley Orgánica 1/1990, de 3 de octubre)</b></td>
+              <td className="px-4 py-2">FOL GS LOGSE</td>
+            </tr>
+            {/* tercer fol */}
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"><b> FOL GM (LOE Ley Orgánica 2/2006, de 3 de mayo)</b></td>
+              <td className="px-4 py-2">FOL GM LOE</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"> </td>
+              <td className="px-4 py-2">FOL GS LOE</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"> </td>
+              <td className="px-4 py-2">FOL GM LOGSE + Certificado nivel básico actividad preventiva</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"> </td>
+              <td className="px-4 py-2">FOL GS LOGSE + Certificado nivel básico actividad preventiva</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"> </td>
+              <td className="px-4 py-2">FTítulo Técnico Superior en Prevención de Riesgos Profesionales</td>
+            </tr>
+            {/* cuarto fol */}
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"> <b>FOL GS (LOE Ley Orgánica 2/2006, de 3 de mayo)</b></td>
+              <td className="px-4 py-2">FOL GM LOE</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"></td>
+              <td className="px-4 py-2">FOL GS LOE</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"></td>
+              <td className="px-4 py-2">FOL GM LOGSE + Certificado nivel básico actividad preventiva</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"></td>
+              <td className="px-4 py-2">FOL GS LOGSE + Certificado nivel básico actividad preventiva</td>
+            </tr>
+            <tr className="border-b ">
+              <td className="px-4 py-2 bg-blue-200"></td>
+              <td className="px-4 py-2">Título Técnico Superior en Prevención de Riesgos Profesionales</td>
+            </tr>
+
+        </tbody>
+      </table>
+        </div>
+
+      </div>{/* Fin del div principal */}
       <Nav posicion={6} />
     </div>
   );
--- src/app/1_2_donde_encontrar_que_modulos_son_convalidables/page.jsx
+++ src/app/1_2_donde_encontrar_que_modulos_son_convalidables/page.jsx
@@ -1,12 +1,44 @@
 import Titulo from "@/components/titulo";
 import Nav from "@/components/nav";
+import Image from "next/image";
 export const metadata = {
   title: "1.2 Dónde encontrar qué modulos son convalidables",
-};export default function Home() {
+}; export default function Home() {
   return (
     <div>
-      <Nav posicion={5} />
+
       <Titulo>{metadata.title}</Titulo>
+      <div className="mt-8 ml-4">
+        <div>
+          <p>Son dos los documentos que debes consultar:</p>
+        </div>
+        <div>
+          <p className="mt-4 ml-8">
+            ✔️Por un lado, los diferentes Anexos del Real Decreto 1085/2020, de 9 de diciembre,
+            por el que se establecen convalidaciones de módulos profesionales de los títulos de Formación Profesional
+            del sistema educativo español y las medidas para su aplicación, y se modifica el Real Decreto 1147/2011, de
+            29 de julio, por el que se establece la ordenación general de la formación profesional del sistema educativo.
+            Aquí puedes consultar las tablas de convalidaciones del Real decreto 1085/2020, de 9 de diciembre
+            (tablas de convalidación).
+          </p>
+          <p className="mt-4 ml-8">
+          ✔️Por otro, el Anexo IV del Real Decreto que crea el título LOE que estás estudiando, ya que establece las convalidaciones con módulos LOGSE del mismo Ciclo. 
+            Quien haya superado la totalidad de un ciclo formativo LOGSE no podrá aportarlo para solicitar la convalidación de ningún módulo profesional del ciclo formativo LOE que lo sustituye. 
+            Estas convalidaciones están reservadas para quien transita a un ciclo formativo LOE desde un ciclo formativo LOGSE, sin haberlo finalizado ni obtenido el título.
+          </p>
+        </div>
+        <p className="mt-8">Ten en cuenta que los <b>módulos profesionales con el mismo código y denominación</b> presentes en diferentes ciclos formativos LOE <b>son en realidad el mismo módulo profesional.</b> Estos módulos se denominan <b>transversales</b> . Si superas un módulo profesional transversal en un ciclo formativo, lo tendrás superado en cualquier otro del que también forme parte y 
+        <b>la calificación obtenida se traslada de un ciclo al otro sin necesidad de convalidación ni más trámite</b>.</p>
+        <p>Los módulos profesionales de FOL y EIE tienen en todos los ciclos formativos LOE la misma denominación, pero diferentes códigos. Por tanto, no hay arrastre de calificación sino convalidación. Se deben cursar o convalidar de un ciclo formativo a otro (siempre que se cumplan los requerimientos para ello).</p>
+          <div>
+            <p className="ml-8">✔️La convalidación del módulo FOL puedes consultarla en el siguiente enlace: <a href="" className="underline text-blue-600">Convalidación de FOL</a>.</p>
+            <p className="ml-8">✔️La convalidación del módulo EIE puedes consultarla en el siguiente enlace: <a href="" className="underline text-blue-600">Convalidación de EIE</a>.</p>
+          </div>
+      <div className="mx-auto flex justify-center w-[400px] h-[300px]">
+        <Image layout='responsive'src='/pollo-con-gafas.jpg' alt="imagen balanza" width="50" height="50"></Image>
+      </div>
+      </div>
+      <Nav posicion={5} />
     </div>
-    )
+  )
 };
